e8211b404fed3d648af53d22864a42d4c4ddef12
socbaycff <socbaycff@gmail.con>
2020-11-11 22:21:28 +0700

    update note

--- Main.js
+++ Main.js
@@ -1,31 +1,32 @@
 'use_strict'
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer();
-var app = express();
+// khoi tao thu vien
+var express = require('express'); // khoi tao express js web app
+var bodyParser = require('body-parser'); // khoi tao thu vien phân giải json
+var multer = require('multer'); // khoi tao thu vien phan giai form data cua html
+var upload = multer(); 
+var app = express(); // khoi chay web app express js
 
-// resouce file 
-app.use(express.static(__dirname + '/'));
+// khai bao thu muc chua resouce file  
+app.use(express.static(__dirname + '/')); // thu muc goc (root)
 
-// for parsing application/json
+// su dung bo phan giai application/json
 app.use(bodyParser.json()); 
 
-// for parsing application/xwww-
+// su dung bo phan giai application/xwww-
 app.use(bodyParser.urlencoded({ extended: true })); 
 //form-urlencoded
 
-// for parsing multipart/form-data
+// su dung bo phan giai multipart/form-data
 app.use(upload.array()); 
 app.use(express.static('public'));
 
 
-// include router
+// lien ket router dieu huong
 var router = require('./router.js');
 app.use('/',router)
 
 // chay server
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 5000, () => { // port 500 dung de test , port con lai dung chay heroku
     console.log('Server running')
 });
--- router.js
+++ router.js
@@ -1,37 +1,42 @@
+// khoi tao cac thu vien
+var express = require('express'); // khoi chay web app express js
+var router = express.Router(); // lien ket router
+var admin = require('firebase-admin'); // thu vien firebase
+var serviceAccount = require("./privateKey.json"); // khoi tao file ket noi server firebase
+var fs = require('fs') // thu vien cho phep doc file html 
+const axios = require('axios') // thu vien cho phep tao http request
+const { Converter, Alarm } = require('./Alarm.js'); // import file Alarm.js vao
 
-var express = require('express');
-var router = express.Router();
-var admin = require('firebase-admin');
-var serviceAccount = require("./privateKey.json");
-var url = require('url')
-var fs = require('fs')
-const axios = require('axios')
-const { Converter, Alarm } = require('./Alarm.js');
-
+// server key cho firebase
 var serverKey = 'key=AAAAhO2bFYw:APA91bEzd3Rmnym3ln5nrDshVka0JuCLDnPJV7lY142_0JKhGJSHVel35nC-L2NzBXsvLK_WzTYEvJCDQoHHJX_EH6ivFb8q2y4xR8AqTRbQoaWJYnQ4sosH-k-3WwNethe0jIVWwWFS';
 
+// khoi tao firebase 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://remotealarmclock-2f98a.firebaseio.com"
 });
 
-var token = "fZgGBrzpLBA:APA91bGr1JXwvKMqXmTu6bvfzm1LgL6VKinszgq_nO7_wyN91qCiyiljqdGc9TJHki95MIvBUFYGeLHMYg39gJl2ifS4leCmx6EtwkCc1K7dvviqVDk2EptfxD6LC5o3XIwXMDbG3XjL"
+//var token = "fZgGBrzpLBA:APA91bGr1JXwvKMqXmTu6bvfzm1LgL6VKinszgq_nO7_wyN91qCiyiljqdGc9TJHki95MIvBUFYGeLHMYg39gJl2ifS4leCmx6EtwkCc1K7dvviqVDk2EptfxD6LC5o3XIwXMDbG3XjL"
 
+// khoi tao config gui thong bao cho client (Firebase Cloud messsaging)
 let config = {
   headers: {
-    'Content-Type': 'application/json',
-    'Authorization': serverKey
+    'Content-Type': 'application/json', // noi dung header la json
+    'Authorization': serverKey // xac thuc bang server key dc cung cap
   }
 }
 
-router.get('/', function (req, res) {
-  fs.readFile('Home.html', function (err, data) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    return res.end();
+///////////////////////////////////////////////mapping trang chu 
+router.get('/', function (req, res) { // url request
+  fs.readFile('Home.html', function (err, data) { // file dc tra ve cho user laf home.html
+    res.writeHead(200, { 'Content-Type': 'text/html' }); // http code 200 ok
+    res.write(data);  // gui ve client
+    return res.end(); // ket thuc
   });
 });
 
+
+///////////////////////////////////////////////mapping trang about
 router.get('/AboutUs.html', function (req, res) {
   fs.readFile('AboutUs.html', function (err, data) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -39,7 +44,7 @@ router.get('/AboutUs.html', function (req, res) {
     return res.end();
   });
 });
-
+///////////////////////////////////////////////mapping trang lost
 router.get('/Lost.html', function (req, res) {
   fs.readFile('Lost.html', function (err, data) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -48,6 +53,7 @@ router.get('/Lost.html', function (req, res) {
   });
 });
 
+///////////////////////////////////////////////mapping trang dang nhap (for fun)
 router.get('/SignIn.html', function (req, res) {
   fs.readFile('SignIn.html', function (err, data) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -55,7 +61,7 @@ router.get('/SignIn.html', function (req, res) {
     return res.end();
   });
 });
-
+///////////////////////////////////////////////mapping trang dang ky (for fun)
 router.get('/SignUp.html', function (req, res) {
   fs.readFile('SignUp.html', function (err, data) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -63,21 +69,18 @@ router.get('/SignUp.html', function (req, res) {
     return res.end();
   });
 });
-///////////////////////////////////////////////mapping update alarm
+
+
+///////////////////////////////////////////////////////////////mapping update alarm
 router.post('/update.html', function (req, res) {
-  console.log('da vao trong post update route')
-
-
-  let data = req.body
-
-  var time = data.time.toString()
-  console.log(data.title.hour)
-  let hour = parseInt(time.substring(0, 2))
+  let data = req.body // lay data gui len tu form 
+  var time = data.time.toString()  // name cua input la time <input name="time" >
+  let hour = parseInt(time.substring(0, 2)) // cat chuoi lay gio
   let minute = parseInt(time.substring(3, 5))
-  let title = data.title
-  // send request to server
-  let id = data.aid
+  let title = data.title // // name cua input la title <input name="title" >
 
+  // khoi tao data gui thong bao den mobile
+  let id = data.aid
   let json = {
     "to": "/topics/remoteAlarm",
     "data": {
@@ -89,7 +92,7 @@ router.post('/update.html', function (req, res) {
     },
     "priority": "high"
   }
-  axios.post('https://fcm.googleapis.com/fcm/send', json, config)
+  axios.post('https://fcm.googleapis.com/fcm/send', json, config)  // gui http post den mobile
     .then(res => {
       console.log(`statusCode: ${res.statusCode}`)
       //console.log(res)
@@ -101,6 +104,7 @@ router.post('/update.html', function (req, res) {
       console.error(error)
     })
 
+    // cap nhat data len firebase
   admin.firestore().collection("alarms").doc(id.toString()).update({
     alarmId: parseInt(id),
     hour: hour,
@@ -109,14 +113,16 @@ router.post('/update.html', function (req, res) {
     title: title
   });
 
+  // dieu huong ve home 
   res.redirect('/Home.html');
 });
 
-///////////////////////////////////////////////mapping cancel alarm
+///////////////////////////////////////////////////////////////mapping cancel alarm
 router.get('/cancel/:id', function (req, res) {
-  console.log('da vao trong cancel route')
+  // tuong tu o tren, chuan bi data gui thong bao den mobile
+  // roi cap nhat database tren firebase
+
   let id = req.params.id
-  console.log(id)
   let json = {
     "to": "/topics/remoteAlarm",
     "data": {
@@ -128,7 +134,7 @@ router.get('/cancel/:id', function (req, res) {
   axios.post('https://fcm.googleapis.com/fcm/send', json, config)
     .then(res => {
       console.log(`statusCode: ${res.statusCode}`)
-      //console.log(res)
+
     })
     .catch(error => {
       console.error(error)
@@ -139,22 +145,17 @@ router.get('/cancel/:id', function (req, res) {
   res.redirect('/Home.html');
 });
 
-////////////////////////////////////////////////mapping new alarm
+////////////////////////////////////////////////////////////////mapping new alarm
 router.post('/new.html', function (req, res) {
-  console.log('da vao trong post new')
   // chuan bi data
   var id = Math.floor(100000000 + Math.random() * 900000000);
   console.log(id)
   let data = req.body
-
   var time = data.time.toString()
   console.log(data.title.hour)
   let hour = parseInt(time.substring(0, 2))
   let minute = parseInt(time.substring(3, 5))
   let title = data.title
-  // send request to server
-
-
   let json = {
     "to": "/topics/remoteAlarm",
     "data": {
@@ -166,7 +167,7 @@ router.post('/new.html', function (req, res) {
     },
     "priority": "high"
   }
-  axios.post('https://fcm.googleapis.com/fcm/send', json, config)
+  axios.post('https://fcm.googleapis.com/fcm/send', json, config) // gui den mobile http post
     .then(res => {
       console.log(`statusCode: ${res.statusCode}`)
       //console.log(res)
@@ -175,12 +176,13 @@ router.post('/new.html', function (req, res) {
       console.error(error)
     })
 
-  // add to firestore
+  // cap nhat firestore
   admin.firestore().collection("alarms").doc(id.toString())
     .withConverter(Converter)
     .set(new Alarm(id, hour, minute, title, true));
 
+  // dieu huong ve home
   res.redirect('/Home.html');
 });
 
-module.exports = router
+module.exports = router // xuat router ra cho Main.js su dung
