565d9edd04ab7fd9fb5db9405083a9052cf4c6e3
khangle99 <dev.khangle@gmail.com>
2021-05-10 17:42:52 +0700

    add new UI validate field

--- assets/js/firebaseUtil.js
+++ assets/js/firebaseUtil.js
@@ -13,90 +13,6 @@ firebase.initializeApp(firebaseConfig);
 
 var db = firebase.firestore();
 const list = document.querySelector('.thumbnails');
-// $( document ).ready(function() {
-//     console.log("ready----")
-  
-    
-//     console.log(alarmsList)
-
-
-//   });
-/*
-db.collection("alarms").onSnapshot(function (querySnapshot) {
-    list.innerHTML = "";
-    var alarms = [];
-    querySnapshot.forEach(function (doc) {
-        if (doc.data().monday) {
-            console.log("true")
-        } else {
-            console.log("false")
-        }
-        alarms += `<div class="box">
-        <br>
-        <!-- Color: Green = đã đặt báo thức  -->
-        <i id="${doc.data().alarmId}" onclick="changeColor(${doc.data().alarmId})" class="fa fa-clock-o"
-            style="font-size: 8rem; color: ${doc.data().started ? "#008000" : "#808080"};" aria-hidden="true"></i>
-        <div class="inner">
-            <h3>${doc.data().hour} : ${doc.data().minute}</h3>
-            <p>${doc.data().title}</p>
-
-            <p>
-                <span id="${doc.data().alarmId}_mo" onclick="dateboxColor(id)"
-                style="cursor: pointer; font-weight: bold;font-size: 14px;line-height: 35px ; width: 35px; border: 2px solid;display: inline-block;color:  ${doc.data().monday ? "#008000" : "#808080"}; text-align: center;border-radius: 4px;">Mon</span>
-                <span id="${doc.data().alarmId}_tu" onclick="dateboxColor(id)"
-                style="cursor: pointer; font-weight: bold;font-size: 14px;line-height: 35px ; width: 35px; border: 2px solid;display: inline-block;color:  ${doc.data().tuesday ? "#008000" : "#808080"};text-align: center;border-radius: 4px;">Tue</span>
-                <span id="${doc.data().alarmId}_we" onclick="dateboxColor(id)"
-                style="cursor: pointer; font-weight: bold;font-size: 14px;line-height: 35px ; width: 35px; border: 2px solid;display: inline-block;color:  ${doc.data().wednesday ? "#008000" : "#808080"};text-align: center;border-radius: 4px;">Wed</span>
-                <span id="${doc.data().alarmId}_th" onclick="dateboxColor(id)"
-                style="cursor: pointer; font-weight: bold;font-size: 14px;line-height: 35px ; width: 35px; border: 2px solid;display: inline-block;color:  ${doc.data().thursday ? "#008000" : "#808080"};text-align: center;border-radius: 4px;">Thu</span>
-                <span id="${doc.data().alarmId}_fr" onclick="dateboxColor(id)"
-                style="cursor: pointer; font-weight: bold;font-size: 14px;line-height: 35px ; width: 35px; border: 2px solid;display: inline-block;color:  ${doc.data().friday ? "#008000" : "#808080"};text-align: center;border-radius: 4px;">Fri</span>
-                <span id="${doc.data().alarmId}_sa" onclick="dateboxColor(id)"
-                style="cursor: pointer; font-weight: bold;font-size: 14px;line-height: 35px ; width: 35px; border: 2px solid;display: inline-block;color:  ${doc.data().saturday ? "#008000" : "#808080"};text-align: center;border-radius: 4px;">Sat</span>
-                <span id="${doc.data().alarmId}_su" onclick="dateboxColor(id)"
-                style="cursor: pointer; font-weight: bold;font-size: 14px;line-height: 35px ; width: 35px; border: 2px solid;display: inline-block;color:  ${doc.data().sunday ? "#008000" : "#808080"};text-align: center;border-radius: 4px;">Sun</span>
-            </p>
-            
-
-            <!-- Checkbox Repeat-->
-			<div>
-				<div style="text-align: center;">
-					<div class="onoffswitch">
-						Repeat
-						<br>
-						<input type="checkbox"  ${doc.data().recurring ? "checked" : ""} name="onoffswitch" class="onoffswitch-checkbox" 
-							id="s${doc.data().alarmId}" onclick="ChuMiNga(${doc.data().alarmId})">
-						<label class="onoffswitch-label" for="s${doc.data().alarmId}">
-							<div class="onoffswitch-inner">
-								<div class="onoffswitch-active">
-									<div class="onoffswitch-switch">ON</div>
-								</div>
-								<div class="onoffswitch-inactive">
-									<div class="onoffswitch-switch">OFF</div>
-								</div>
-							</div>
-						</label>
-					</div>
-				</div>
-			</div>
-			<!-- Checkbox Repeat-->
-
-      
-       
-
-            <button class="btn success" style="font-size: 20px" onclick="openUpdateForm(${doc.data().alarmId})">Set Time</button>
-            <div style="line-height:50%;">
-                <br>
-            </div>
-            <button class="btn danger" style="font-size: 20px" onclick="location.href='cancel/${doc.data().alarmId}'">Remove</button>
-        </div>
-    </div>`
-    });
-
-    list.innerHTML = alarms;
-});
-*/
-
 
 function ChuMiNga(id) {
     
@@ -187,9 +103,6 @@ function dateboxColor(id) {
 
 }
 
-
-
-
 function openUpdateForm(id) {
     $("#update").show()
     $("#alarmMid").val(id)
@@ -204,6 +117,12 @@ function openUpdateForm(id) {
     $('#alarmID_th1').prop('checked', isCheck(`#${id}_th`))
     $('#alarmID_su1').prop('checked', isCheck(`#${id}_su`))
     $('#alarmId_Repeat_Inpopupform4').prop('checked', $(`#${id} #recurring`).is(':checked'))
+}
+
+function openDeleteForm(id) {
+    $("#delete").show()
+    var idString = id.toString();
+    document.getElementById('deleteForm').action = "/cancel/" + idString;
 
 }
 
@@ -215,32 +134,32 @@ function isCheck(id) {
     }
 }
 
-function changeColor(id) {
-    var flag = false;
+// function changeColor(id) {
+//     var flag = false;
 
-    if ($("#" + id).css('color') == 'rgb(0, 128, 0)') {
-        $("#" + id).css('color', 'rgb(128, 128, 128)')
-        flag = false
-        console.log("tat")
-    } else {
-        $("#" + id).css('color', 'rgb(0, 128, 0)')
+//     if ($("#" + id).css('color') == 'rgb(0, 128, 0)') {
+//         $("#" + id).css('color', 'rgb(128, 128, 128)')
+//         flag = false
+//         console.log("tat")
+//     } else {
+//         $("#" + id).css('color', 'rgb(0, 128, 0)')
 
-        flag = true
-        console.log("bat")
-    }
-
-
-    var ref = db.collection("alarms").doc(String(id));
+//         flag = true
+//         console.log("bat")
+//     }
 
 
-    ref.update({
-        started: flag
-    })
-        .then(function () {
-            console.log("Document successfully updated!");
-        })
-        .catch(function (error) {
+//     var ref = db.collection("alarms").doc(String(id));
 
-            console.error("Error updating document: ", error);
-        });
-}
+
+//     ref.update({
+//         started: flag
+//     })
+//         .then(function () {
+//             console.log("Document successfully updated!");
+//         })
+//         .catch(function (error) {
+
+//             console.error("Error updating document: ", error);
+//         });
+// }
